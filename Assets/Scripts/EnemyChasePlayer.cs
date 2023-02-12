de58d41f09a1416979a2cfd90a6e218e3c8e29de
Josh <joshua.guptill@mymail.champlain.edu>
2023-02-12 15:46:25 -0500

    Added Blue Enemry Chase player AI

---
+++ Assets/Scripts/EnemyChasePlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChasePlayer : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform myTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward*5*Time.deltaTime);
+    }
+}
